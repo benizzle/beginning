d8c6726ec7ec9a988df1b14c52f9d06cba2ccf73
Eifert, Benjamin <benjamin.eifert@tecalliance.net>
2023-09-13 11:28:27 +0200

    added data types

--- Program.cs
+++ Program.cs
@@ -1,21 +1,54 @@
-﻿namespace HelloWorld
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Frage den Nutzer nach der ersten Zahl
-            Console.WriteLine("Nenne eine Zahl!");
-            string zahl1 = Console.ReadLine();
+            string text = "Das ist ein Text";
+            Console.WriteLine(text);
+            int zahl = 21;
+            Console.WriteLine(zahl);
+            double kommazahl = 21.3;
+            Console.WriteLine(kommazahl);
+            float weitereFlieskommaZahl = 21.333F;
+            Console.WriteLine(weitereFlieskommaZahl);
+            decimal flieskomma = 24.4M;
+            Console.WriteLine(flieskomma);
             
-            //Fragen den Nutzer nach der zweiten Zahl
-            Console.WriteLine("Nenne eine Zahl die du von " + zahl1 + " subtrahieren willst!");
-            string zahl2 = Console.ReadLine();
+            bool wahrheitswert = true;
+            Console.WriteLine(wahrheitswert);
+            bool anders = false;
+            Console.WriteLine(anders);
+            
+            char buchstabe = (char)48;
+            Console.WriteLine(buchstabe);
 
-            int ergebnis = Convert.ToInt16(zahl1) - Convert.ToInt16(zahl2);            
-  
-            //Gebe das Ergebnis auf der Console aus SUBTRAKTION
-            Console.WriteLine($"Dein Ergbnis: {zahl1} - {zahl2} = {ergebnis}");
+            //ASCII Chart 48 = 0
+            long langezahl = 123456753156456456;
+            Console.WriteLine(langezahl);
+            short kurzezahl = 30000;
+            Console.WriteLine(kurzezahl);
+
+            uint positivezahl = 123;
+            Console.WriteLine(positivezahl);
+
+            
+            string[] myArray = new string[10];
+            myArray[0] = "erster Wert";
+            myArray[9] = "letzter Wert";
+
+            Console.WriteLine(myArray[0]);
+            Console.WriteLine(myArray[9]);
+
+            List<string> myList = new List<string>();
+            
+            myList.Add("Benni");
+
+            Console.WriteLine(myList[0]);
+
         }
     }
 }
