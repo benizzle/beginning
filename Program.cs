a2751f571d419c533167b1ab28986c1c2b1a8b5d
Eifert, Benjamin <benjamin.eifert@tecalliance.net>
2023-09-13 14:13:02 +0200

    added c# examples

--- Program.cs
+++ Program.cs
@@ -7,97 +7,27 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            /*
-            int myNum = 9;
-            double myDoubleNum = 8.99;
-            char myLetter = 'A';
-            bool myBoolean = false;
-            string myText = "Hello World";
-            */
+            string example_text = " This is a test ";
 
-            /*
-            float zahl1 = 24.2314f;
-            float zahl2 = 1.12222223262313f;
-            float ergebnis = zahl1 + zahl2;
+            Console.WriteLine(example_text.ToLower());
+            Console.WriteLine(example_text.ToUpper());
+            Console.WriteLine(example_text.Trim());
+            Console.WriteLine(example_text.Contains(This));
+            Console.WriteLine(example_text.CopyTo());
+            Console.WriteLine(example_text.Count());
+            Console.WriteLine(example_text.EndsWith(" "));
+            Console.WriteLine(example_text.Equals(" This is a test "));
+            Console.WriteLine(example_text.Insert(5, "ABCD"));
+            Console.WriteLine(example_text.OrderBy());
+            Console.WriteLine(example_text.PadLeft(30, '_'));
+            Console.WriteLine(example_text.Remove(3, 6));
+            Console.WriteLine(example_text.Replace('i', 'ä'));
+            Console.WriteLine(example_text.Reverse().ToArray());
+            Console.WriteLine(example_text.Substring(5, 10));
 
-            Console.WriteLine($"Mein Ergebnis ist {zahl1} + {zahl2} = {ergebnis}");
-
-
-            int x,y,z;
-            x = y = z = 50;
-
-            Console.WriteLine(x + y + z);
-            */
-
-            /*
-            int ganzZahl = 0;
-            double myDouble = 5.5;
-            Console.WriteLine(myDouble);
-            ganzZahl = (int)myDouble;
-            Console.WriteLine(ganzZahl);
-
+            int example_number = 56;
+            example_number.equals();
             
-            double doubleZahl = 3.56666665647383857;
-            float floatZahl = 0;
-            Console.WriteLine(doubleZahl);
-            floatZahl = float(doubleZahl);
-            Console.WriteLine(doubleZahl);
-            */
-
-            /*
-            int zahl1 = 50;
-            int zahl2 = 100;
-
-            int ergebnis1 = zahl1 + zahl2;
-            int ergebnis2 = zahl1 - zahl2;
-
-            int ergebnis3 = zahl1 * zahl2;
-            int ergebnis4 = zahl1 / zahl2;
-
-            int ergebnis5 = zahl1 % zahl2;
-
-            zahl1++;
-
-            zahl2--;
-
-            zahl1 += 10;
-            */
-
-            int zahl1 = 50;
-            int zahl2 = 30;
-            int zahl3 = 200;
-
-            int ergebnis1 = zahl2 + zahl1;
-            Console.WriteLine(ergebnis1);
-            
-            // ++ vor Variable erhöht zuerst und ++ nach Zahl wird erst zugewiesen und dann erhöht
-            int ergebnis2 = ++zahl1;
-            Console.WriteLine(ergebnis2);
-
-            int ergebnis3 = zahl1 % zahl2;
-            Console.WriteLine(ergebnis3);
-
-            int ergebnis4 = zahl3 % zahl1;
-            Console.WriteLine(ergebnis4);
-
-            int ergebnis5 = zahl2 + zahl2 * 2;
-            Console.WriteLine(ergebnis5);
-
-            int ergebnis6 = (zahl2 + zahl2) * 2;
-            Console.WriteLine(ergebnis6);
-
-            double double1 = 50.50;
-            double double2 = 30.30;
-
-            double doubleergebnis = double1 / double2;
-            Console.WriteLine(doubleergebnis);
-
-            float float1 = 50.50f;
-            float float2 = 30.30f;
-
-            double floatergebnis = float1 / float2;
-            Console.WriteLine(floatergebnis);
-
         }
     }
 }
