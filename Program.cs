6b6f4f485902dcdd06f0da8a584d2076ad61e73c
Eifert, Benjamin <benjamin.eifert@tecalliance.net>
2023-09-13 09:52:23 +0200

    added alter in Zehn Jahren

--- Program.cs
+++ Program.cs
@@ -6,8 +6,14 @@
         {
             Console.WriteLine("Gebe mir deinen Namen!");
             string name = Console.ReadLine();
+
+            Console.WriteLine("Sage mir wie alt du bist?");
+            string age = Console.ReadLine();
             
-            Console.WriteLine("Hello, " + name);
+            int alter = Convert.ToInt16(age);
+            int alterInZehn = alter + 10;
+
+            Console.WriteLine("Hello, " + name + "! Dein Alter ist " + age + ". In 10 Jahren bist du " + alterInZehn );
         }
     }
 }
