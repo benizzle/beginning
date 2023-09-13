878377e5a12586daf860d3b454613a3675c24a3c
Eifert, Benjamin <benjamin.eifert@tecalliance.net>
2023-09-13 14:47:10 +0200

    added for each

--- Program.cs
+++ Program.cs
@@ -7,9 +7,27 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            /*For Loop - For Schleife
             for(int zaehlvariable=0;zaehlvariable<10;zaehlvariable++)
             {
                 Console.WriteLine(zaehlvariable);
+            }
+            */
+
+            //For Each
+            string[] emails = new string[]
+            {
+                "ben@tecalliance.net",
+                "abc@tecalliance.net",
+                "xyz@tecalliance.net"
+            };
+            foreach(string email in emails)
+            {
+                Console.WriteLine($"Die Email lautet: {email}");
+            }
+            for(int position=0;position<emails.Length;position++)
+            {
+                Console.WriteLine($"Die Email lautet: {emails[position]}");
             }
         }
     }
