1d8dded87d885c81ac1a556daf62a550a0d35598
Eifert, Benjamin <benjamin.eifert@tecalliance.net>
2023-09-13 10:00:10 +0200

    added string interpolation

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@
             int alter = Convert.ToInt16(age);
             int alterInZehn = alter + 10;
 
-            Console.WriteLine("Hello, " + name + "! Dein Alter ist " + age + ". In 10 Jahren bist du " + alterInZehn );
+            Console.WriteLine($"Hello, {name}! Dein Alter ist {age} In 10 Jahren bist du {alterInZehn}");
         }
     }
 }
