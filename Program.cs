d5b5387645a870fd4aca6d97b59bc9fa7e1642e8
Eifert, Benjamin <benjamin.eifert@tecalliance.net>
2023-09-14 14:26:25 +0200

    Aufgabe 11

--- Program.cs
+++ Program.cs
@@ -8,33 +8,24 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //Aufgabe 1: Initialisiere 2 Zahlen und gebe die Summe auf der Console aus
-            //Aufgabe 2: Lasse den Nutzer zwei Zahlen eingeben und gebe die Summe auf der Console aus
-            //Aufgabe 3: Lasse den Nutzer eine Zahl eingeben und gebe alle Zahlen
-            //           bis einschließlich der eingegeben Zahl aus
-            //Aufgabe 4: Lasse den Nutzer eine Zahl eingeben und gebe nur die geraden Zahlen aus.
-            //           (Hinweis: Modulo operator. => Geteilt durch 2,
-            //                  wenn der Rest 0 ist, ist die Zahl gerade)
-            //Aufgabe 5: Lasse den Nutzer eine Zahl eingeben und berechne von dieser Zahl die Fakultät.
-            //Aufgabe 6a: Lasse den Nutzer einen Text eingeben und gebe den Text nur in Großbuchstaben aus
-            //Aufgabe 6b: Gebe die Länge des Textes aus
-            //Aufgabe 6c: Zähle wie oft der Buchstabe a vorkommt.
-            //Aufgabe 6d: Gebe nur einen Teil des Textes aus. Ab Position 3, und dann die nächsten 5 Buchstaben
+            //Aufgabe 11: Frage den Nutzer nach einer Zahl und zähle von dieser Rückwärts bis 0
+            //Aufgabe 12: Laufe mit einer For-Schleife über die Namen der Azubis & Studenten. Wenn der Name des Azubis ein „e“ enthält, gebe den Namen aus, ansonsten nicht.
+            //Aufgabe 12b: Wiederhole das Ganze und lass den Nutzer einen Buchstaben bestimmen, nach dem gesucht wird.
+            //Aufgabe 13: Frage den Nutzer  wie viele Einträge er in einem int-Array haben möchte. Initialisiere ein int-Array von dieser Größe und befülle dieses Int Array in einer Schleife, 
+            //              indem du den Nutzer jedes Mal frägst, welche Zahl eingetragen werden soll.  
+            //              Gebe anschließend alle Zahlen nacheinander nochmal aus
+            //Aufgabe 13b: Gehe das Int-array durch und gebe den Maximal-Wert aus, Minimal-Wert und den Durchschnitt.
 
-            //Aufgabe 7: Frage den Nutzer nach einem Text und ersetze im Text alle vorkommenden „U“ und „u“ durch „G“ und „g“
-            //Aufgabe 8: Frage den Nutzer nach einer Texteingabe und fülle diese mit bis zu 20 Stellen mit einem y von links auf
-            //Aufgabe 9: Frage den Nutzer nach einer Eingabe mit Semikolons ( ; ) und Splitte die Eingabe an diesem Zeichen. Gebe die einzelnen Werte auf der Console aus.
-            //Aufgabe 10: Frage den Nutzer nach einer Eingabe und überprüfe die Texteingabe des Nutzers, ob die Zeichen „abc“ in der Eingabe enthalten sind. Gebe true oder false auf der Konsole aus.
+            //A11
+
+            Console.WriteLine("Nenne eine Zahl!");
+            int zahl = Convert.ToInt16(Console.ReadLine());
             
-            //A10
-
-            Console.WriteLine("Schreibe einen tollen Text!");
-            string eingabe = Console.ReadLine();
-
-            bool ergebnis = eingabe.Equals("abc");
-
-            Console.WriteLine(ergebnis);
-
+            while(zahl >= 0)
+            {
+                Console.WriteLine($"Folge: {zahl}");
+                zahl--;
+            }
         }
     }
 }
