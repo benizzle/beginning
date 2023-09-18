c3b33269d0d9d1ab6a20e8a299d1e6bc5c1e1d21
Eifert, Benjamin <benjamin.eifert@tecalliance.net>
2023-09-18 14:05:27 +0200

    Split für Buchstaben zählen

--- Program.cs
+++ Program.cs
@@ -9,39 +9,15 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //Erstelle ein array das Zahlen beinhaltet
-            //Es sollen 5 Zahlen eingegeben werden können
-            //Befülle das Array in einer Schleife durch den Nutzer
-            //Gebe am Ende die:
-            //  - die kleinste Zahl aus
-            //  - die größte Zahl 
-            //  - den Durchschnitt
+            Console.WriteLine("Gebe einen Text ein");
+            string eingabe = Console.ReadLine();
+            
+            Console.WriteLine(eingabe.Length);
 
-            int zahl = 5;
+            string[] splittedText = eingabe.Split('h');
 
-            int[] arr = new int[zahl];
 
-            for(int i=0; i<arr.Length; i++)
-            {
-                Console.WriteLine($"Befülle Feld {i+1} von {zahl}");
-                arr[i] = Convert.ToInt32(Console.ReadLine());
-            }
-
-            Console.WriteLine("Welche Zahl willst du ausgeben? Kleinste, Groesste oder den Durchschnitt?");
-            string txt = Console.ReadLine();
-            if(txt == "Kleinste")
-            {
-                Console.WriteLine(arr.Min());
-            }
-            else if(txt == "Groesste")
-            {
-                Console.WriteLine(arr.Max());
-            }
-            else if(txt == "Durchschnitt")
-            {
-                Console.WriteLine(arr.Average());
-            }
-
+            Console.WriteLine(splittedText.Length - 1);
         }
     }
 }
