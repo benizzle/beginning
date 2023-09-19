a629811d2c2cacca1d572fc3e72250963756791e
Eifert, Benjamin <benjamin.eifert@tecalliance.net>
2023-09-19 08:26:41 +0200

    Sekunden zählen

--- Program.cs
+++ Program.cs
@@ -8,17 +8,30 @@ namespace HelloWorld
         {
             //Nutzer soll Zahl eingeben. Anzahl an Sekunden
             //In Tage Stunden Minuten Sekunden konvertieren
-            //90.061 = 1 tag 1 stunde 1 minute 1 sekunde 86400
-
+            //90.061 = 1 tag 1 stunde 1 minute 1 sekunde
+            /*
             Console.WriteLine("Gebe eine Zahl ein!");
             int eingabe = Convert.ToInt32(Console.ReadLine());
 
-            int tag = eingabe / 86400;
-            int stunde = (eingabe / 3600) - tag;
-            int minute = (eingabe / 60) - (stunde*60);
+            int tage = eingabe / 86400;
+            int stunde = (eingabe - (tage*86400)) / 3600;
+            int minute = ((eingabe - (tage*86400)) - (stunde*3600)) / 60;
             int sekunde = eingabe % 60;
 
-            Console.WriteLine($"Ergebnis: {tag} Tage {stunde} Stunden {minute} Minuten und {sekunde} Sekunden");
+            Console.WriteLine($"Ergebnis: {tage} Tage {stunde} Stunden {minute} Minuten und {sekunde} Sekunden");
+            */
+
+            Console.WriteLine("Gebe eine Zahl ein!");
+            int amountSeconds = Convert.ToInt32(Console.ReadLine());
+
+            int amountDays = amountSeconds / 24 / 60 / 60;
+            int restSeconds = amountSeconds % (24 * 60 * 60);
+            int amountHours = restSeconds / 60 / 60;
+            restSeconds = restSeconds % (60 * 60);
+            int amountMinutes = restSeconds / 60;
+            restSeconds = restSeconds % 60;
+
+            Console.WriteLine($"Ergebnis: {amountDays} Tage {amountHours} Stunden {amountMinutes} Minuten und {restSeconds} Sekunden");
         }
     }
 }
